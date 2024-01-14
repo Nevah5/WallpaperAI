2f44eea89c5d8da7c5b4e4b9d55c85134163093f
Nevah5 <neevaah5@gmail.com>
2024-01-14 17:39:28 +0100

    Removed CreatePageViewModel

--- app/src/main/java/dev/nevah5/uek355/wallpaper_ai/create_page/CreatePageFragment.kt
+++ app/src/main/java/dev/nevah5/uek355/wallpaper_ai/create_page/CreatePageFragment.kt
@@ -1,30 +1,5 @@
 package dev.nevah5.uek355.wallpaper_ai.create_page
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import dev.nevah5.uek355.wallpaper_ai.R
 
-class CreatePageFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = CreatePageFragment()
-    }
-
-    private lateinit var viewModel: CreatePageViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_create_page, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CreatePageViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
-}
+class CreatePageFragment : Fragment()
--- app/src/main/java/dev/nevah5/uek355/wallpaper_ai/create_page/CreatePageViewModel.kt
+++
@@ -1,7 +0,0 @@
-package dev.nevah5.uek355.wallpaper_ai.create_page
-
-import androidx.lifecycle.ViewModel
-
-class CreatePageViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
