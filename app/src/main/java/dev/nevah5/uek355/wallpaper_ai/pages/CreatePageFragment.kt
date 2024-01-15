d0a01a7d71c9e48e47a9a3b3398a5adde029c87d
Nevah5 <neevaah5@gmail.com>
2024-01-15 10:51:51 +0100

    Updated styling of api key input field on login activity

--- app/src/main/java/dev/nevah5/uek355/wallpaper_ai/pages/CreatePageFragment.kt
+++ app/src/main/java/dev/nevah5/uek355/wallpaper_ai/pages/CreatePageFragment.kt
@@ -1,16 +1,21 @@
 package dev.nevah5.uek355.wallpaper_ai.pages
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dev.nevah5.uek355.wallpaper_ai.LoginActivity
 import dev.nevah5.uek355.wallpaper_ai.R
+
 
 class CreatePageFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("test")
+
+        val intent = Intent(requireActivity(), LoginActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onCreateView(
